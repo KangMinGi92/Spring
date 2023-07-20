d4731b4a1aea3f766d0c03ed0d181cd163bd7f5e
kkangmingi <alsrlwpdhkd@gmail.com>
2023-07-20 16:56:58 +0900

    2023.07.20 spring boot 커밋

---
+++ firstboot/src/main/java/com/bs/boot/firstboot/controller/BasicController.java
@@ -0,0 +1,26 @@
+package com.bs.boot.firstboot.controller;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class BasicController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "하이하이하이!";
+	}
+	
+	@RequestMapping("/test")
+	public List<String> test(){
+		log.info("우와우와");
+		return List.of("1","2","3","4","5");
+	}
+
+}
---
+++ helloboot/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>boot 메인화면</title>
+</head>
+<body>
+	<h2>나의 첫 boot화면</h2>
+
+</body>
+</html>
