74048b50a99ef09e32b3f8bb81249427f4afc0b6
kkangmingi <alsrlwpdhkd@gmail.com>
2023-07-24 16:53:33 +0900

    2023.07.24 스프링 security 커밋

--- spring/src/main/java/com/bs/spring/member/model/dto/Member.java
+++ spring/src/main/java/com/bs/spring/member/model/dto/Member.java
@@ -1,14 +1,20 @@
 package com.bs.spring.member.model.dto;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,7 +25,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Member {
+public class Member implements UserDetails{
 	
 	@NotEmpty(message = "아이디는 반드시 입력하세요!")
 	@Size(min=4, message = "아이디는 4글자 이상 입력하세요!")
@@ -28,7 +34,7 @@ public class Member {
 			, regexp="(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[~!@#$%^&*()])[a-zA-Z~!@#$%^&*()]{8,}")
 	private String password;
 	
-	private String userName;
+	private String name;
 	
 	private String gender;
 	@Min(value=14, message = "14살 이상만 가능합니다.")@Max(value=150, message = "")
@@ -43,4 +49,50 @@ public class Member {
 	private String[] hobby;
 	
 	private Date enrolldate;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		//로그인한 사용자의 권한을 설정하는 메소드
+			List<GrantedAuthority> auth=new ArrayList();
+			auth.add(new SimpleGrantedAuthority("ROLE_USER"));
+			if(userId.equals("admin")) {
+				//auth.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+				auth.add(new SimpleGrantedAuthority("admin"));
+			}else if(userId.equals("user04")) {
+				auth.add(new SimpleGrantedAuthority("manager"));
+			}
+		return auth;
+	}
+
+	@Override
+	public String getUsername() {
+		// 인증할 id값을 반환해주는 함수
+		return this.userId;
+	}
+
+	//아래 인증확인절차는 DB에 있으므로 true 로 반환하게 강제시킴 default(false)
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
 }
--- spring/src/main/webapp/WEB-INF/views/common/header.jsp
+++ spring/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -2,7 +2,8 @@
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
-<c:set var="path" value="${pageContext.request.contextPath }"/>	
+<c:set var="path" value="${pageContext.request.contextPath }"/>
+<c:set var="loginInfo" value="${sessionScope.SPRING_SECURITY_CONTEXT.authentication.principal}"/>
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,6 +29,11 @@
 </head>
 <body>
 	<div id="container">
+		<p>
+			시큐리티에 저장된 로그인 정보 확인하기<br>
+			${sessionScope.SPRING_SECURITY_CONTEXT.authentication.principal }<br>
+			${sessionScope.SPRING_SECURITY_CONTEXT.authentication.principal==null}<br>
+		</p>
 		<header>
 			<div id="header-container">
 				<h2>${param.title}</h2>
@@ -59,13 +65,20 @@
 							<a class="nav-link" href="${path}/board/boardList.do">게시판</a>
 						</li>
 					</ul>
-					<c:if test="${not empty loginMember }">
-						<span><a href="${path }/member/mypage.do?userId=${loginMember.userId}"><c:out value="${loginMember.userName }"/></a>님,환영합니다.</span>
+					<%-- <c:if test="${not empty loginMember }"> --%>
+					<c:if test="${loginInfo!=null }">
+						<%-- <span><a href="${path }/member/mypage.do?userId=${loginMember.userId}"> --%>
+						<%-- <c:out value="${loginMember.userName }"/></a>님,환영합니다.</span> --%>
+						<span>
+							<a href="${path }/member/mypage.do?userId=${loginInfo.username}">
+							<c:out value="${loginInfo.username }"/></a>님,환영합니다.
+						</span>
 						<button class="btn btn-outline-primary my-2 my-sm-0" onclick="chattingOpen();">채팅하기</button>
 						<button class="btn btn-outline-dark my-2 my-sm-0" 
-						onclick="location.replace('${path}/member/logout.do')">로그아웃</button>
+						onclick="location.replace('${path}/seculogout.do')">로그아웃</button>
 					</c:if>
-					<c:if test="${empty loginMember }">
+					<%-- <c:if test="${empty loginMember }"> --%>
+					<c:if test="${loginInfo==null }">
 						<button class="btn btn-outline-success my-2 my-sm-0"
 							data-toggle="modal" data-target="#loginModal">로그인</button>
 						&nbsp;
@@ -87,7 +100,7 @@
 							<span aria-hidden="true">&times;</span>
 						</button>
 					</div>
-					<form action="${path }/member/login.do" method="post">
+					<form action="${path }/loginEnd" method="post">
 						<div class="modal-body">
 							<input type="text" name="userId" class="form-control"
 							placeholder="아이디입력" required><br/>
